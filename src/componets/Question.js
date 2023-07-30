7139866d383da512693b1cbe720504c09a6ab7f3
Majid Ali <majidwp69.gmail.com>
2023-07-30 10:51:56 +0500

    complete react quiz

---
+++ src/componets/Question.js
@@ -0,0 +1,11 @@
+import React from "react";
+import Options from "./Options";
+
+export default function Question({ question, dispatch, answer }) {
+  return (
+    <div>
+      <h3>{question.question}</h3>
+      <Options question={question} dispatch={dispatch} answer={answer} />
+    </div>
+  );
+}
